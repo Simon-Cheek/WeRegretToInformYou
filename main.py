f700d292b0b6741d738534292d3cad538b56e5f4
Simon Cheek <simon.cheek@yahoo.com>
2024-02-17 12:28:45 -0700

    Basic Pygame experimentation

--- main.py
+++ main.py
@@ -3,24 +3,50 @@ import pygame
 pygame.init()
 
 
-screen = pygame.display.set_mode((1280,720))
+screen = pygame.display.set_mode((1280, 720))
+pygame.display.set_caption("We Regret to Inform You")
 clock = pygame.time.Clock()
 running = True
+
+# starting location of rectangle
+x = 100
+y = 100
+height = 100
+width = 200
+vel = 3
 
 
 # game CYCLE / LOOP
 while running:
+    # sets each loop to run every 1/10 of a second
+    pygame.time.delay(10)
 
+    # starts the event loop
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
     screen.fill("black")
+    pygame.draw.rect(screen, (255, 0, 0), (x, y, height, width))
+    pygame.display.update()
 
-    # RENDER THE GAME HERE
+    # find keys that are pressed
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        x -= vel
 
-    pygame.display.flip()
+    if keys[pygame.K_RIGHT]:
+        x += vel
 
-    clock.tick(60)
+    if keys[pygame.K_UP]:
+        y -= vel
 
+    if keys[pygame.K_DOWN]:
+        y += vel
+
+    if keys[pygame.K_ESCAPE]:
+        running = False
+
+
+# after the main loop
 pygame.quit()
